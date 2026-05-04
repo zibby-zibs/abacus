0ffd9989bcb3e1c4c5030652cbd53251f8696347
zibby-zibs <oloyedetimi@gmail.com>
2026-05-04 14:27:20 +0100

    Update WhatsApp URL in constants.ts for better user engagement

--- src/components/landing/constants.ts
+++ src/components/landing/constants.ts
@@ -1,5 +1,4 @@
-export const WHATSAPP_URL =
-	"https://wa.me/2349092179152?text=I%20want%20to%20get%20started%20with%20Abacus";
+export const WHATSAPP_URL = "https://wa.me/2348100969457?text=Hi%20Abakus";
 
 export const MARQUEE_ITEMS = [
 	"Expense tracking",
